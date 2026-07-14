8ace24e0ef9a8277237ffc38757f94167a5f1feb
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-07-15 01:19:40 +0600

    fix: all TypeScript build errors resolved

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,17 @@
 import { authClient } from "./auth-client";
 
 export const getFeaturedDoctor = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/doctors`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/doctors`,{
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 };
 
 export const getDoctorByID = async (id: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-doctors/${id}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-doctors/${id}`,{
+    cache: "no-store",
+  });
   const data = await res.json();
   return {
     ...data,
@@ -19,21 +23,23 @@ export const getDoctorByID = async (id: string) => {
 };
 
 export const getAllReview = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review`,{
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 };
 
 export const getTimeSlot = async (doctorId: string, selectedDate: string) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/appointments/slots/${doctorId}/${selectedDate}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/appointments/slots/${doctorId}/${selectedDate}`,{cache:"no-store"}
   );
   const data = await res.json();
   return data;
 };
 
 export const getAllDoctors = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-doctors`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-doctors`,{cache:"no-store"});
   const data = await res.json();
   return data;
 };
@@ -41,7 +47,7 @@ export const getAllDoctors = async () => {
 export const getUserData = async () => {
   const { data: session } = await authClient.getSession();
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/appointments/user/${session?.user.id}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/appointments/user/${session?.user.id}`,{cache:"no-store"}
   );
   const data = await res.json();
   return data
--- src/proxy.ts
+++ src/proxy.ts
@@ -3,9 +3,8 @@ import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
   const session =
-    request.cookies.get("better-auth.session_token")?.value ||
-    request.cookies.get("__Secure-better-auth.session_token")?.value;
-
+  request.cookies.get("better-auth.session_token")?.value ||
+  request.cookies.get("__Secure-better-auth.session_token")?.value;
   if (!session) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
